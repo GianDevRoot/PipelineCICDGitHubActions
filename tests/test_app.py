a9a75a1c5b17dbd29a5da17ff78b9f83b9ab76a0
Giancarlos <giancarlosmamanibenitez@gmail.com>
2025-08-05 07:33:07 -0500

    rename flask_app to app

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ import os
 # Añadir el directorio padre al path (para GitHub Actions)
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from flask_app import app
+from app import app
 
 def test_homepage():
     tester = app.test_client()
