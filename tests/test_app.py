053cc0150a3d005aa37aafb983134bc2bb22f763
Giancarlos <giancarlosmamanibenitez@gmail.com>
2025-08-05 07:23:26 -0500

    fix test import

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,6 @@
 from flask_app import app
 
 def test_homepage():
-    """
-    Verifica que la ruta raíz devuelva el texto esperado.
-    """
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
